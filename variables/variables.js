be1a53cd4217f4b2a4b9b0b51d2f09308d16a7ac
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-09-10 13:21:57 -0400

    add answers to exercises 3-6

---
+++ variables/variables.js
@@ -0,0 +1,11 @@
+// Exercise 3
+// The code outputs an error because foo is declared in a block and is not accessible in the global scope. In other words, foo does not exist outside of the curly braces.
+
+// Exercise 4
+// The code runs the three logged greetings and then outputs an error on line 6. This is because variables declared with const cannot be reassigned.
+
+// Exercise 5
+// The program logs "bar" to the console because foo is initialized to "bar" in the global scope. Another variable named foo is initialized in the block in line 3, but that variable is not accessible outside of the block.
+
+// Exercise 6
+// The code runs without error despite the declaration of two constants with the same name because they are declared in different scopes and therefore the block scoped "FOO" variable is a different variable altogether than the one declared in the global scope.
