0d0d0533a203486f95bc0e7264c383e4b8aa79b9
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-09-13 19:05:32 -0400

    add excercise 1 & 2 solutions

---
+++ input_output/greeter.js
@@ -0,0 +1,9 @@
+// Exercise 1
+let readline = require("readline-sync");
+// let name = readline.question("What is your name? ");
+// console.log(`Hello, ${name}!`);
+
+// Exercise 2
+let firstName = readline.question("What is your first name? ");
+let lastName = readline.question("What is your last name? ");
+console.log(`Hello ${firstName} ${lastName}`);
