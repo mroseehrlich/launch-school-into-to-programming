d035245b550e42d582e82e8f54940df53014bfe8
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-09-13 19:52:31 -0400

    add exercies 4 and 5

--- functions/functions.js
+++ functions/functions.js
@@ -1,4 +1,8 @@
 // Exercise 1
 // The code logs 1 to the console. The bar variable that is logged in line 7 is the one declared in line 1. The second bar variable in line 3 is unknown to the rest of the program because functions are block scoped.
 
-//Exercise 2
+//Exercise 4
+// The code does not log anything to the console. The return statement in line 4 ends the function execution, preventing the console.log in line 4 from running.
+
+//Exercise 5
+// The code doesn't log anything to the console. It only returns the value of the words argument concatenated with the "!!!!".
